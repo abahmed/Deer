10fbc64ae30295aa10e0107f0e252ed7f287ddcf
Abdelrhman Ahmed <a.ahmed1026@gmail.com>
2017-11-30 16:43:25 +0200

    Issue #1: Allow generating tests

--- make.py
+++ make.py
@@ -36,6 +36,16 @@ def clean_build():
 def print_usage():
 	print ("Usage: make.py <command>")	
 
+def print_help():
+	print_usage()
+	print ("This is a list of supported commands:")
+	print ("    build\t\tBuilds Deer project.")
+	print ("    clean\t\tRemove existing build")
+	print ("    run  \t\tRuns project after building it.")
+	print ("    test\t\tBuilds tests of Deer project.")
+	print ("    run-test\t\tRuns tests after building it.")
+	print ("    add-test NAME\tAdds a template of a new test.")
+
 def main():
 	# Check argument if it is passed properly.
 	if len(sys.argv) < 2:
@@ -50,39 +60,57 @@ def main():
 	if command == 'build':
 		print("Building project...")
 		execute_command("mkdir build; cd build; cmake ..; make -j" + 
-						 str(num_jobs))
+				str(num_jobs))
 	elif command == 'run':
 		print("Running project...")
 		executable = "./build/bin/Deer"
 		if not os.path.exists(executable):
 			raise ValueError("Error: Executable program does not exist")
-		else:
-			execute_command(executable)
+		execute_command(executable)
 	elif command == 'clean':
 		print("Cleaning project...")
 		clean_build()
 	elif command == 'test':
 		print("Building tests...")
 		execute_command("mkdir build_test; cd build_test; \
-						 cmake -Dtest=ON ..; make -j" + str(num_jobs))
+				 cmake -Dtest=ON ..; make -j" + str(num_jobs))
 	elif command == 'run-test':
 		print("Running test...")
 		test_dir = "build_test"
 		if not os.path.exists(test_dir):
 			raise ValueError("Error: Test directory does not exist")
-		else:
-			execute_command("cd build_test; make test")
+		execute_command("cd build_test; make test")
+	elif command == 'new-test':
+		if len(sys.argv) != 3:
+			print_help()
+			exit(0)
+		test_name = "test_" + sys.argv[2]
+		test_dir = "test/" + test_name
+		if os.path.exists(test_dir):
+			raise ValueError("Error: This test is already existing.")
+		
+		# Create testing directory		
+		execute_command("cd test; mkdir " + test_name)
+	
+		# Adding test to CMakeList.txt
+		execute_command("cd test; echo 'add_subdirectory(" + test_name +
+				")' >> CMakeLists.txt")
+		# Generate template files
+		execute_command("cd " + test_dir + "; echo '" +
+		"add_executable(run_" + test_name + " " + test_name + ".cpp)\n\n" +
+		"# Standard linking to gtest stuff.\n" +
+		"target_link_libraries(run_" + test_name + " gtest gtest_main)\n\n" +
+		"# This is so you can do 'make test' to see all tests run.\n" +
+		"add_test(NAME " + test_name + " COMMAND run_" + test_name + 
+		")' > CMakeLists.txt")
+		execute_command("cd " + test_dir + "; echo '" +
+		"#include \"gtest/gtest.h\"' > " + test_name + ".cpp")	
 	elif command == '--help':
-		print_usage()
-		print ("This is a list of supported commands:")
-		print ("    build\tBuilds Deer project.")
-		print ("    clean\tRemove existing build")
-		print ("    run\tRuns project after building it.")
-		print ("    test\tBuilds tests of Deer project.")
-		print ("    run-test\tRuns tests after building it.")
+		print_help()
 	else:
 		# This argument is not matched.
-		print ("make.py: '{}' is not a make command. See 'make.py --help'.".format(command))
+		print ("make.py: '{}' is not a make command. See 'make.py " +
+		       "--help'.".format(command))
   
 
 if __name__== "__main__":
