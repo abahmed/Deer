84c5ca26841b3837fa4a589973de045d1620774b
Ahmed Ismail <ahmedismmz@gmail.com>
2018-01-08 15:33:12 +0200

    Issue 10: Set application icon

--- src/gui.h
+++ src/gui.h
@@ -18,10 +18,12 @@ class deer::deer_gui {
     GtkBuilder *builder;
     GObject *window;
     GObject *menu_quit_button;
+    GdkPixbuf *icon;
 
  public:
     deer_gui();
     void load_gui(int argc, char **argv);
+    GdkPixbuf* create_pixbuf(const gchar *filename);
     static void destroy(GtkWidget *widget, gpointer data);
     ~deer_gui();
 };
