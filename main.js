fb11ec0ba8cf8f5c86a465513f6fab3253d015ad
mmoanis <mohamed_moanis@outlook.com>
2018-01-28 19:39:34 +0200

    Directory Structure
    - Moving icons to images folder.
    - Move electron from the dev dependencies list.

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ function createWindow () {
 
   // and load the index.html of the app.
   win.loadURL(url.format({
-    pathname: path.join(__dirname, 'app/index.html'),
+    pathname: path.join(__dirname, 'public/index.html'),
     protocol: 'file:',
     slashes: true
   }))
