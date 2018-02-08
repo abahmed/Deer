7c1b23da61fb0fed7f4fcf6a0f09a99377b1c77e
Nitesh Sawant <niteshsawant023@gmail.com>
2018-02-08 23:55:07 +0530

    changed browser resolution

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({ width: 720, height: 400  })
 
   // and load the index.html of the app.
   win.loadURL(url.format({
