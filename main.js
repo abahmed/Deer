a491d8a702cd8b08fa432884779bafd38d69b953
Austin F <austindanielfrench@gmail.com>
2018-03-05 20:04:06 -0800

    (main.js) fixed the window size so that it's no longer resizable

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({ width: 720, height: 400  })
+  win = new BrowserWindow({ width: 720, height: 400, resizable: false  })
 
   // and load the index.html of the app.
   win.loadURL(url.format({
