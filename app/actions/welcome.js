f8bf62679abd5ac975139be683e5848eb942da47
ionxeph <cheng0807@gmail.com>
2018-06-23 12:19:45 -0500

    fix #84 adding function to clear out timer

--- app/actions/welcome.js
+++ app/actions/welcome.js
@@ -7,6 +7,8 @@ export const getNextLang = createAction(ACTIONS.GET_NEXT_LANG)
 // Used for inverting values of fadeIn.
 export const toggleFade = createAction(ACTIONS.TOGGLE_FADE)
 
+// Setting the timer to a variable so to be able to stop after navigating away from Welcome component.
+let timer = 0
 // Async action that is fired from time to time update fadeIn boolean and
 // index, so UI is rerendered with next values.
 export const updateLang = () => (dispatch, getState) => {
@@ -15,8 +17,13 @@ export const updateLang = () => (dispatch, getState) => {
   let isFadeIn = state.fadeIn
   if (isFadeIn) { dispatch(getNextLang()) }
 
-  setTimeout(() => {
+  timer = setTimeout(() => {
     dispatch(toggleFade())
     dispatch(updateLang())
   }, isFadeIn ? 3000 : 180)
+}
+
+// Stopping the timer to call updateLang
+export const stopUpdateLang = () => () => {
+  clearTimeout(timer)
 }
