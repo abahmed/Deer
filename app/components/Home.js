b8ea2ccd25937be13db46d5b65b4288c16230237
Abdelrhman Ahmed <a.ahmed1026@gmail.com>
2018-08-12 20:32:08 +0200

    Fix space is required before closing bracket in line 23

--- app/components/Home.js
+++ app/components/Home.js
@@ -20,7 +20,7 @@ export default () => {
       <div className='container-fluid row flex-xl-nowrap'>
         <div className='col-12 col-md-3 col-xl-3 home-sidebar' />
         <div className='col-12 col-md-9 col-xl-9 home-content'>
-          <Route exact path='/' component={HomeContent}/>
+          <Route exact path='/' component={HomeContent} />
         </div>
       </div>
     </div>
