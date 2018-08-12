19545489fe311903bb76d76294603c9eea678d9c
Abdelrhman Ahmed <a.ahmed1026@gmail.com>
2018-08-12 19:20:28 +0200

    Fixes #89: Adds layout of home component (#102)

--- app/components/Home.js
+++ app/components/Home.js
@@ -16,6 +16,10 @@ export default () => {
   return (
     <div>
       <Header />
+      <div className='container-fluid row flex-xl-nowrap'>
+        <div className='col-12 col-md-3 col-xl-3 home-sidebar' />
+        <div className='col-12 col-md-9 col-xl-9 home-content' />
+      </div>
     </div>
   )
 }
