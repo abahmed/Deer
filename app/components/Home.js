f45e1c954f830ce662bd24b44af2b05a3a9bf1cd
Abdelrhman Ahmed <a.ahmed1026@gmail.com>
2018-08-12 20:41:34 +0200

    Fixes #103: Implement HomeContent component (#104)
    - Implement HomeContent component
    - Add default route to be loaded when Deer loads

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Redirect } from 'react-router-dom'
+import { Redirect, Route } from 'react-router-dom'
 import Header from './Header'
+import HomeContent from './HomeContent'
 import {
   checkRedirectToWelcomePage,
   setNotFirstTimeFlag
@@ -18,7 +19,9 @@ export default () => {
       <Header />
       <div className='container-fluid row flex-xl-nowrap'>
         <div className='col-12 col-md-3 col-xl-3 home-sidebar' />
-        <div className='col-12 col-md-9 col-xl-9 home-content' />
+        <div className='col-12 col-md-9 col-xl-9 home-content'>
+          <Route exact path='/' component={HomeContent} />
+        </div>
       </div>
     </div>
   )
