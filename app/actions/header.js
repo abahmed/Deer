1f8282807ec31176a566dde51284fa68306931a0
Abdelrhman Ahmed <a.ahmed1026@gmail.com>
2018-08-31 02:49:28 +0200

    Fixes #119: Add save button (#120)

--- app/actions/header.js
+++ app/actions/header.js
@@ -3,3 +3,6 @@ import { ACTIONS } from '../constants/actions'
 
 // Used for updating state of adding new note button (e.g. enabled or disabled).
 export const setNewNoteDisabled = createAction(ACTIONS.UPDATE_NEW_NOTE_STATE)
+
+// Used for updating state of save button (e.g. enabled or disabled).
+export const setSaveDisabled = createAction(ACTIONS.UPDATE_SAVE_NOTE_STATE)
--- app/components/NoteEditor.js
+++ app/components/NoteEditor.js
@@ -3,12 +3,43 @@ import { Editor } from 'draft-js'
 import PropTypes from 'prop-types'
 
 export default class NoteEditor extends Component {
+  constructor (props) {
+    // Initialize this using super
+    super()
+
+    this.onEditorChange = this.onEditorChange.bind(this)
+  }
+
+  onEditorChange (newEditorState) {
+    const currentContent = this.props.editorState.getCurrentContent()
+    const newContent = newEditorState.getCurrentContent()
+    if (currentContent !== newContent) {
+      // There is a change in content
+      if (newContent.hasText()) {
+        // The new content has text, so we will enable save button.
+        this.props.setSaveDisabled(false)
+      } else {
+        // The new content is empty, so we will disable new Note and save
+        // buttons.
+        this.props.setNewNoteDisabled(true)
+        this.props.setSaveDisabled(true)
+      }
+    }
+
+    this.props.updateEditorState(newEditorState)
+  }
+
+  componentWillUnmount () {
+    // Disable save button as editor will be unmounted.
+    this.props.setSaveDisabled(true)
+  }
+
   render () {
     return (
       <div className='NoteEditor'>
         <Editor
           editorState={this.props.editorState}
-          onChange={this.props.onSaveEditorState}
+          onChange={this.onEditorChange}
           placeholder='Write down your thoughts...'
         />
       </div>
@@ -17,6 +48,8 @@ export default class NoteEditor extends Component {
 }
 
 NoteEditor.propTypes = {
-  onSaveEditorState: PropTypes.func.isRequired,
-  editorState: PropTypes.object.isRequired
+  editorState: PropTypes.object.isRequired,
+  setSaveDisabled: PropTypes.func.isRequired,
+  setNewNoteDisabled: PropTypes.func.isRequired,
+  updateEditorState: PropTypes.func.isRequired
 }
