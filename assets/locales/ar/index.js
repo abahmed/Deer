3a951996a5d2cea6abdbc7d84deea4e9d1eb6ff2
Abdelrhman Ahmed <a.ahmed1026@gmail.com>
2018-10-09 07:16:40 +0200

    Fixes #144: Add settings component  (#157)

--- assets/locales/ar/index.js
+++ assets/locales/ar/index.js
@@ -3,11 +3,13 @@ import homeContent from './homeContent.json'
 import noteEditor from './noteEditor.json'
 import statusModal from './statusModal.json'
 import yesNoModal from './yesNoModal.json'
+import settings from './settings.json'
 
 export default {
   header,
   homeContent,
   noteEditor,
   statusModal,
-  yesNoModal
+  yesNoModal,
+  settings
 }
