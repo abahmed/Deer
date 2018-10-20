b8b5e6bacb99ea48d83adbf627384dc4164cfbee
Ricardo Bibieca <shirotokuro3715@gmail.com>
2018-10-20 15:31:58 -0400

    Issue_167: User choose language

--- app/constants/welcome.js
+++ app/constants/welcome.js
@@ -2,51 +2,61 @@ export const welcome = [
   {
     lang: 'English',
     welcome: 'Welcome',
-    nextBtn: 'Next'
+    nextBtn: 'Next',
+    selectLang: 'Select your language'
   },
   {
     lang: 'Arabic',
     welcome: 'أهلا بك',
-    nextBtn: 'التالى'
+    nextBtn: 'التالى',
+    selectLang: 'اختر لغتك'
   },
   {
     lang: 'French',
     welcome: 'Bienvenue',
-    nextBtn: 'Prochain'
+    nextBtn: 'Prochain',
+    selectLang: 'Choisissez votre langue'
   },
   {
     lang: 'Spanish',
     welcome: 'Bienvenido',
-    nextBtn: 'Siguiente'
+    nextBtn: 'Siguiente',
+    selectLang: 'Elige tu idioma'
   },
   {
     lang: 'Italian',
     welcome: 'Benvenuto',
-    nextBtn: 'Il prossimo'
+    nextBtn: 'Il prossimo',
+    selectLang: 'Seleziona la tua lingua'
   },
   {
     lang: 'German',
     welcome: 'Willkommen',
-    nextBtn: 'Nächster'
+    nextBtn: 'Nächster',
+    selectLang: 'Wähle deine Sprache'
   },
   {
     lang: 'Russian',
     welcome: 'Добро пожаловать',
-    nextBtn: 'Далее'
+    nextBtn: 'Далее',
+    selectLang: 'Выберите ваш язык'
   },
   {
     lang: 'Chinese',
     welcome: '欢迎',
-    nextBtn: '下一个'
+    nextBtn: '下一个',
+    selectLang: '选择你的语言'
   },
   {
     lang: 'Japanese',
     welcome: 'ようこそ',
-    nextBtn: '次'
+    nextBtn: '次',
+    selectLang: 'あなたが使う言語を選んでください'
   },
   {
     lang: 'Hindi',
     welcome: 'स्वागत हे',
-    nextBtn: 'आगामी'
+    nextBtn: 'आगामी',
+    selectLang: 'अपनी भाषा का चयन करें'
   }
 ]
--- app/containers/Welcome.js
+++ app/containers/Welcome.js
@@ -1,19 +1,19 @@
 import { connect } from 'react-redux'
 import WelcomePage from '../components/Welcome'
-import {
-  updateLang,
-  stopUpdateLang
-} from '../actions/welcome'
+import { updateLang, stopUpdateLang } from '../actions/welcome'
+import { saveSettings } from '../actions/settings'
 
 const mapStateToProps = state => ({
   index: state.welcomeReducer.index,
   fadeIn: state.welcomeReducer.fadeIn,
-  langList: state.welcomeReducer.langList
+  langList: state.welcomeReducer.langList,
+  settingsStatus: state.settingsReducer.settingsStatus
 })
 
 const mapDispatchToProps = dispatch => ({
   updateLang: () => dispatch(updateLang()),
-  stopUpdateLang: () => dispatch(stopUpdateLang())
+  stopUpdateLang: () => dispatch(stopUpdateLang()),
+  saveSettings: newSettings => dispatch(saveSettings(newSettings))
 })
 
 export default connect(
