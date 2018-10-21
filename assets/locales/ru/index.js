87f96d56fdf4337712e70090a966cfe0d80a6e33
artywhite <artywhite@users.noreply.github.com>
2018-10-21 19:42:56 +0300

    Fixes #187: Added russian localization (#186)

---
+++ assets/locales/ru/index.js
@@ -0,0 +1,15 @@
+import header from './header.json'
+import homeContent from './homeContent.json'
+import noteEditor from './noteEditor.json'
+import statusModal from './statusModal.json'
+import yesNoModal from './yesNoModal.json'
+import settings from './settings.json'
+
+export default {
+  header,
+  homeContent,
+  noteEditor,
+  statusModal,
+  yesNoModal,
+  settings
+}
