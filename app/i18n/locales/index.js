081c2c354908ddbb47be9b72bcfbf76000a50f8c
Johannes Przymusinski <johannes@przymusinski.de>
2019-03-22 19:53:43 +0100

    Added german language support (#242)
    added support for the german language
    you can decide if you prefer (du) or (sie)

--- app/i18n/locales/index.js
+++ app/i18n/locales/index.js
@@ -1,40 +1,49 @@
 /** @module locales */
 
 /** Supported Languages. */
-export const SUPPORTED_LANGS = [
-  {
-    language: 'English',
-    code: 'en',
-    path: 'en'
-  },
-  {
-    language: 'العربية',
-    code: 'ar',
-    path: 'ar'
-  },
-  {
-    language: 'Español',
-    code: 'es',
-    path: 'es'
-  },
-  {
-    language: 'Русский',
-    code: 'ru',
-    path: 'ru'
-  },
-  {
-    language: 'Polski',
-    code: 'pl',
-    path: 'pl'
-  },
-  {
-    language: 'Português do Brasil',
-    code: 'pt',
-    path: 'pt_br'
-  },
-  {
-    language: 'Česky',
-    code: 'cs',
-    path: 'cs'
-  }
+export const SUPPORTED_LANGS = [{
+        language: 'English',
+        code: 'en',
+        path: 'en'
+    },
+    {
+        language: 'Deutsch (Du)',
+        code: 'de_du',
+        path: 'de_du'
+    },
+    {
+        language: 'Deutsch (Sie)',
+        code: 'de_sie',
+        path: 'de_sie'
+    },
+    {
+        language: 'العربية',
+        code: 'ar',
+        path: 'ar'
+    },
+    {
+        language: 'Español',
+        code: 'es',
+        path: 'es'
+    },
+    {
+        language: 'Русский',
+        code: 'ru',
+        path: 'ru'
+    },
+    {
+        language: 'Polski',
+        code: 'pl',
+        path: 'pl'
+    },
+    {
+        language: 'Português do Brasil',
+        code: 'pt',
+        path: 'pt_br'
+    },
+    {
+        language: 'Česky',
+        code: 'cs',
+        path: 'cs'
+    }
 ]
