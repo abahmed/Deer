b86f5c5adda2b3c6f184e92fb5d3d6b7ceef32d1
Lionel Stewart <lionelmstewart@gmail.com>
2019-08-09 12:40:34 -0400

    add noteBookReducer to index.js

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import note from './note'
+import notebook from './notebook'
 import settings from './settings'
 
 // Creates a single root reducer out of Deer's reducers and returns an object
 // whose values are reducing functions with keys passed.
 export default combineReducers({
   noteReducer: note,
+  noteBookReducer: notebook,
   settingsReducer: settings
 })
